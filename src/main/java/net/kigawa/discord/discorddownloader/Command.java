4590db269bec3a3b7e712533be507d9ab5befb26
kigawa <kigawa.8390@gmail.com>
2022-05-01 18:49:11 +0900

    fix something

--- src/main/java/net/kigawa/discord/discorddownloader/Command.java
+++ src/main/java/net/kigawa/discord/discorddownloader/Command.java
@@ -3,8 +3,10 @@ package net.kigawa.discord.discorddownloader;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageHistory;
+import net.kigawa.kutil.kutil.KutilFile;
 import net.kigawa.kutil.log.log.Logger;
 
+import java.io.File;
 import java.util.concurrent.ExecutionException;
 
 public class Command extends com.jagrosh.jdautilities.command.Command
@@ -36,13 +38,53 @@ public class Command extends com.jagrosh.jdautilities.command.Command
                 if (history == null) return;
                 var list = history.getRetrievedHistory();
 
-                if (list.isEmpty()) return;
+                if (list.isEmpty()) {
+                    logger.info("end download");
+                    return;
+                }
 
                 for (Message message : list) {
-                    logger.info(message.getContentRaw());
+                    download(message);
                 }
                 latestMessageId = list.get(list.size() - 1).getIdLong();
             }
         });
+    }
+
+    private void download(Message message)
+    {
+        DiscordDownloader.getExecutor().execute(() -> {
+            var time = message.getTimeCreated();
+            File channelDir = KutilFile.getFile(DiscordDownloader.DOWNLOAD_FILE,
+                    replaceName(message.getChannel().getName())
+            );
+            File dir = KutilFile.getFile(channelDir,
+                    time.getYear() + "-"
+                            + time.getMonth()
+                            + "-" + time.getDayOfMonth()
+            );
+            var attachments = message.getAttachments();
+
+            if (attachments.isEmpty()) return;
+            if (!dir.exists()) {
+                logger.info("create dir " + dir.getName());
+                dir.mkdirs();
+            }
+
+            for (Message.Attachment attachment : attachments) {
+                logger.info("download " + attachment.getFileName());
+                attachment.downloadToFile(KutilFile.getFile(dir,
+                        replaceName(message.getAuthor().getName()) + "-"
+                                + attachment.getFileName()));
+            }
+        });
+    }
+
+    private String replaceName(String str)
+    {
+        var result = str
+                .replace('/', '_')
+                .replace('\\', '_');
+        return result;
     }
 }
